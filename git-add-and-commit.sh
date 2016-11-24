fdc13cabae9a0758e87c61f89d9564b2a37d50bb
bokonist <jayrm1995@gmail.com>
2016-11-25 03:36:19 +0530

    added choice not to push

--- git-add-and-commit.sh
+++ git-add-and-commit.sh
@@ -1,5 +1,11 @@
 #/bin/bash
 	git add $1
 	read -p "Enter the commit message : " commit_message
-	git commit -m  " $commit_message "
-	git push origin master
+	git commit -m " $commit_message "
+	read -p "Push to master? (y/n)" choice
+	y='y'
+	if [ "$choice" = 'y' ]; then
+		git push origin master
+	elif [ "$choice" = 'n' ]; then
+		echo "Not pushed"
+	fi
