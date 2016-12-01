1705a5f8a891c7e252f8e1518d352005b4eb09df
bokonist <jayrm1995@gmail.com>
2016-12-02 04:13:07 +0530

    added option to commit

--- git-add-and-commit.sh
+++ git-add-and-commit.sh
@@ -1,11 +1,16 @@
 #/bin/bash
 	git add $1
-	read -p "Enter the commit message : " commit_message
-	git commit -m " $commit_message "
-	read -p "Push to master? (y/n)" choice
-	y='y'
+	read -p "Do you want to commit? (y/n) : " choice
 	if [ "$choice" = 'y' ]; then
-		git push origin master
+		read -p "Enter the commit message : " commit_message
+		git commit -m " $commit_message "
+		read -p "Push to master? (y/n) " choice
+		if [ "$choice" = 'y' ]; then
+			git push origin master
+		elif [ "$choice" = 'n' ]; then
+			echo "Not pushed"
+		fi
 	elif [ "$choice" = 'n' ]; then
-		echo "Not pushed"
+		echo "Not committed"
+
 	fi
