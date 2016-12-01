f25f0302bfb73994fbf3053aa3b5487555d7b54d
bokonist <jayrm1995@gmail.com>
2016-12-02 04:26:59 +0530

    added options and error messages

--- bashrc-functions/glutcompile.sh
+++ bashrc-functions/glutcompile.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 glutcompile() 
 {
-	g++ -o $1 $2 -lGL -lGLU -lglut
+	if [ $# -eq 1 ]; then
+		if g++ $1 -lGL -lGLU -lglut ; then
+			echo "Outputted to $PWD/a.out"
+		else
+			echo "g++ says error."
+	elif [ $# -eq 2 ]; then		
+		g++ -o $1 $2 -lGL -lGLU -lglut
+		echo "Outputted to $PWD/$2"
+	elif [ $# -eq 0 ]; then
+		echo "Gotta gimme something to compile, pal."
+	fi
 }
