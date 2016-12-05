f087de266e5395d18407ffe88bb0b13c4e14f7d9
bokonist <jayrm1995@gmail.com>
2016-12-06 00:51:30 +0530

    also cleans up a.out

--- cleanup.sh
+++ cleanup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -x
-find . -mindepth 1 -perm /a=x -path ./.git -prune -o -type f -not -name "*.*"  -exec rm -i '{}' '+'
+find . -mindepth 1 -perm /a=x -path ./.git -prune -o -type f -not -name "*.*" -or -name "a.out"  -exec rm -i '{}' '+'
 # finds all regular files in the pwd aleast one folder down, which is exectuable, doesn't look in the ./git folder, 
 # name is not of the form(*.*) which is usually the case for what i want to clean up (cpp obj files) and removes them interactively
